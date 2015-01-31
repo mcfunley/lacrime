75ba136447e0dac4c990101d6619d5c951b53021
Dan McKinley <dm@stripe.com>
2015-01-30 21:55:14 -0800

    download script instead of data in repo

--- download.sh
+++ download.sh
@@ -5,7 +5,6 @@ download() {
 }
 
 mkdir -p data
-download "2004"
 download "2005"
 download "2006"
 download "2007"
@@ -15,5 +14,5 @@ download "2010"
 download "2011"
 download "2012"
 download "2013"
-download "2014"
-
+wget --no-clobber --output-document=data/2014.csv "https://data.lacity.org/api/views/eta5-h8qx/rows.csv?accessType=DOWNLOAD"
+wget --no-clobber --output-document=data/la_city.kml "https://s3-us-west-2.amazonaws.com/mappingla.com/downloads/neighborhoods/la_city.kml"
